15fb64a6c2add7f25abfeff7de895c4ea0ebb2d5
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-11 15:07:34 +0700

    post_acts_as_paranoid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190711074804) do
+ActiveRecord::Schema.define(version: 20190711080213) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20190711074804) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_posts_on_deleted_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
