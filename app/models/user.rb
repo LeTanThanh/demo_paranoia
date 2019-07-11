00d732a4e610974a1a68c4374088883ec3ab9dcd
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-11 14:51:35 +0700

    user_acts_as_paranoid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  acts_as_paranoid
+
   has_many :posts, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190711070534) do
+ActiveRecord::Schema.define(version: 20190711074804) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20190711070534) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_users_on_deleted_at"
   end
 
 end
