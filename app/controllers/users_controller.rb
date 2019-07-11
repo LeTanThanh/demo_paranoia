fa3c29521af1a844355e34853321576c6a583ca3
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-11 14:38:15 +0700

    add_users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: :show
+
   def index
     @users = User.all
+  end
+
+  def show
+    @posts = @user.posts
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+
+    redirect_to root_url unless @user
   end
 end
